2989f67198264fa53837e38daa3d26fef4c8c78e
savagesteel <36926406+savagesteel@users.noreply.github.com>
2018-02-28 16:30:03 +0100

    Added page up and page down keys to function layer

--- Enter-The-Matrix-2018.02/keymap.c
+++ Enter-The-Matrix-2018.02/keymap.c
@@ -1,4 +1,6 @@
 // QMK Firmware Satan GH60 French ISO keymap
+// Keycodes : https://docs.qmk.fm/keycodes.html
+
 #include "satan.h"
 #include "keymap_french.h"
 
@@ -39,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------------------------.
    * |Rst| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   Del |
    * |-----------------------------------------------------------|
-   * |     |   |   | Up|   |   |   |   |   |   |Prt|BL+|Vo+|     |
+   * |     |   |   | Up|   |PUp|   |   |   |   |Prt|BL+|Vo+|     |
    * |-------------------------------------------------------    |
-   * |       |   |Lft|Dwn|Rgt|   |   |   |   |   |   |BL-|Vo-|   |
+   * |       |   |Lft|Dwn|Rgt|PDw|   |   |   |   |   |BL-|Vo-|   |
    * |-----------------------------------------------------------|
    * |    |   |   |   |   |   |   |   |   |   |   |   |          |
    * |-----------------------------------------------------------|
@@ -56,11 +58,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
   #else
-  RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,     KC_F11,  KC_F12,            KC_DELETE, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCREEN, BL_INC,  KC_AUDIO_VOL_UP,   KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    BL_DEC,  KC_AUDIO_VOL_DOWN, KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,           KC_TRNS, \
-  KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                               KC_TRNS,    KC_TRNS, KC_TRNS,           KC_TRNS ),
+  RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,     KC_F11,  KC_F12,            KC_DELETE, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS,  KC_PGUP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCREEN, BL_INC,  KC_AUDIO_VOL_UP,   KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    BL_DEC,  KC_AUDIO_VOL_DOWN, KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,           KC_TRNS, \
+  KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                               KC_TRNS,    KC_TRNS, KC_TRNS,           KC_TRNS ),
   #endif
 };
 
